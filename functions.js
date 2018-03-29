f079ed39e976aa9a1bbbfc2e14baba19564f8129
青木秀仁 <shamrock1976@me.com>
2018-03-29 22:53:56 +0900

    項目を修正。

--- functions.js
+++ functions.js
@@ -13,7 +13,6 @@ function createContent(feature) {
         popupContents += "<tr><td>電話番号　</td><td>" + feature.properties.TEL + "</td></tr>" ;
         popupContents += "<tr><td>住所　</td><td>" + feature.properties.Add1 + feature.properties.Add2 + "</td></tr>" ;
         popupContents += "<tr><td>定員　</td><td>" + feature.properties.Capall + "</td></tr>" ;
-        popupContents += "<tr><td>定員　</td><td>" + feature.properties.Capall + "</td></tr>" ;
         popupContents += "<tr><td>開園　</td><td>" + feature.properties.Open + "</td></tr>" ;
         popupContents += "<tr><td>閉園　</td><td>" + feature.properties.Close_reg + "</td></tr>" ;
         popupContents += "<tr><td>延長　</td><td>" + feature.properties.Close + "</td></tr>" ;
