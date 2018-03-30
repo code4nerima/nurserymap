1413e873ffa2f2f3bd6e96e217ccb50d28d1f034
青木秀仁 <shamrock1976@me.com>
2018-03-30 22:27:04 +0900

    公共交通機関データを追加。

--- custom.js
+++ custom.js
@@ -9,8 +9,95 @@ function onInitialLatLng() {
 
 // onCreate : This function is called when page began.
 function onCreate(map) {
-    L.marker([35.737841　, 139.653912]).addTo(map)
-    .bindPopup('練馬駅');
+    $.getJSON('data/area.geojson', function(data) {
+        L.geoJson(data, {
+            style : function(feature) {
+                return {
+                    fillColor: 'gray',
+                    weight: 2,
+                    opacity: 1,
+                    color: 'black',  //Outline color
+                    fillOpacity: 0.1
+                };
+            }
+        }).addTo(map);
+    });
+
+    var trainIcon = L.icon({
+        iconUrl: 'data/public_transport/train/train_icon.png',
+        iconSize: [30, 30],
+        popupAnchor: [10, 0],
+    });
+
+    $.getJSON('./data/public_transport/train/train_station.geojson', function(data) {
+        L.geoJson(data, {
+            pointToLayer: function(geoJsonPoint, latlng) {
+                return L.marker(latlng).addTo(map)
+            },
+            onEachFeature: function(feature, layer) {
+                var latlng = [
+                    (feature.geometry.coordinates[0][0][1] + feature.geometry.coordinates[0][1][1])/2,
+                    (feature.geometry.coordinates[0][0][0] + feature.geometry.coordinates[0][1][0])/2
+                ] ;
+
+                var marker = L.marker(latlng, {icon : trainIcon}) ;
+
+                marker.addTo(map)
+
+                var popupContents = "<strong>" + feature.properties.駅名 + "</strong><br/>";
+
+                popupContents += feature.properties.運営会社 + "<br/>";
+                popupContents += feature.properties.路線名;
+    
+                marker.bindPopup(popupContents);
+            }
+        }).addTo(map);
+    });
+
+    $.getJSON('./data/public_transport/train/train_line.geojson', function(data) {
+        L.geoJson(data, {
+            style: function(feature) {
+                return {
+                    weight: 2,
+                    opacity: 1,
+                    color: 'green',  //Outline color
+                };
+            }
+        }).addTo(map);
+    });
+
+    var busIcon = L.icon({
+        iconUrl: 'data/public_transport/bus/bus_icon.png',
+        iconSize: [20, 20],
+        popupAnchor: [10, 0],
+    });
+
+    $.getJSON('./data/public_transport/bus/bus_stop.geojson', function(data) {
+        L.geoJson(data, {
+            pointToLayer: function(geoJsonPoint, latlng) {
+                return L.marker(latlng, {icon : busIcon}).addTo(map)
+            },
+            onEachFeature: function(feature, layer) {
+                var popupContents = "<strong>" + feature.properties.P11_001 + "</strong><br/>";
+
+                popupContents += feature.properties.P11_003_1;
+    
+                layer.bindPopup(popupContents);
+            }
+        }).addTo(map);
+    });
+
+    $.getJSON('./data/public_transport/bus/bus_root.geojson', function(data) {
+        L.geoJson(data, {
+            style: function(feature) {
+                return {
+                    weight: 2,
+                    opacity: 1,
+                    color: 'green',  //Outline color
+                };
+            }
+        }).addTo(map);
+    });
 }
 
 // onCreateMarkerIcon : This function is called when marker icon is created.
@@ -29,11 +116,6 @@ function onCreateMarkerIcon() {
 // onMapClick : This function is called when map was clicked.
 function onMapClick(e) {
 	//popup.setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(map);
-}
-
-// onRequireAreaBorder : This function is called when loading area border geoJSON.
-function onRequireAreaBorder() {
-    return './data/13120.json';
 }
 
 // onCraeteMarkerPopup : This function is called when marker is created by Leaflet.
